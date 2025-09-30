7b891a45df1758c9e2e319476334a6411cf7f51d
Silvio Porto <103378951+silvioporto@users.noreply.github.com>
2025-09-30 12:37:20 -0300

    Update sudo_settings.sh

--- sudo_settings.sh
+++ sudo_settings.sh
@@ -1,6 +1,9 @@
 echo "set completion-ignore-case on" >> /etc/inputrc
 
-sudo apt upgrade
-sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt-get install python3-libgpiod -y
+
+sudo systemctl start ssh
+sudo systemctl enable ssh  # to start on boot
